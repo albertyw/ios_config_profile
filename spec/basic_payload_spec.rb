5ed4488dbcfb655c4b769e52eb7304a5b69c9e31
Albert Wang <albert@cellabus.com>
2015-05-04 01:52:26 -0700

    Add tests for BasicPayload

--- spec/basic_payload_spec.rb
+++ spec/basic_payload_spec.rb
@@ -9,6 +9,16 @@ describe IOSConfigProfile::BasicPayload do
     end
   end
 
+  let(:payload) { Payload.new}
+
+  describe "#uuid" do
+    it "returns a uuid" do
+      uuid1 = payload.uuid
+      uuid2 = payload.uuid
+      expect(uuid1).to_not eq uuid2
+    end
+  end
+
   describe '#to_encrypted_payload' do
     let(:payload) { Payload.new }
 
@@ -25,5 +35,19 @@ describe IOSConfigProfile::BasicPayload do
 
     it { is_expected.to be_a IOSConfigProfile::CommandPayload }
     its(:command) { should == payload }
+  end
+
+  describe "#require_attributes" do
+    it "will check for multiple attributes" do
+      payload.send(:require_attributes, :to_plist, :uuid)
+      expect{payload.send(:require_attributes, :to_plist, :asdf)}.to raise_error
+    end
+  end
+
+  describe "#require_attribute" do
+    it "will raise an error if the name doesn't exist" do
+      payload.send(:require_attribute, :to_plist)
+      expect{payload.send(:require_attribute, :asdf)}.to raise_error
+    end
   end
 end
