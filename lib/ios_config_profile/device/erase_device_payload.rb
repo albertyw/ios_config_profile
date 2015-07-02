652817d9f1de1b1e1b5e2af14b63bcf99c9d7ac4
Albert Wang <albert@cellabus.com>
2015-07-02 16:42:37 -0700

    Add EraseDevicePayload

---
+++ lib/ios_config_profile/device/erase_device_payload.rb
@@ -0,0 +1,16 @@
+class IOSConfigProfile::EraseDevicePayload < Hash
+  include IOSConfigProfile::BasicPayload
+
+  def initialize
+    merge! erase_device_payload
+  end
+
+  private
+
+  def erase_device_payload
+    {
+        'RequestType' => 'EraseDevice'
+
+    }
+  end
+end
