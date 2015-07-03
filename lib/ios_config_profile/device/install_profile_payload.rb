c91b14d3840cf9206005e2e1cb42d26fdd16df91
Albert Wang <albert@cellabus.com>
2015-07-03 00:47:50 -0700

    Add InstallProfilePayload

---
+++ lib/ios_config_profile/device/install_profile_payload.rb
@@ -0,0 +1,20 @@
+class IOSConfigProfile::InstallProfilePayload < Hash
+  include IOSConfigProfile::BasicPayload
+
+  attr_accessor :profile
+
+  def initialize(profile)
+    self.profile = profile
+    require_attributes :profile
+    merge! install_profile_payload
+  end
+
+  private
+
+  def install_profile_payload
+    {
+      'RequestType' => 'InstallProfile',
+      'Payload' => StringIO.new(profile)
+    }
+  end
+end
---
+++ spec/device/install_profile_payload_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe IOSConfigProfile::InstallProfilePayload do
+  subject { IOSConfigProfile::InstallProfilePayload }
+
+  describe "#initialize" do
+    it "has the correct payload" do
+      payload = subject.new('asdf')
+      expect(payload['RequestType']).to eq 'InstallProfile'
+      expect(payload['Payload'].gets).to eq 'asdf'
+    end
+  end
+end
