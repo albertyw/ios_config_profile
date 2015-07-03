baf857873c34f9e01ba98f813f27877627cf0805
Albert Wang <albert@cellabus.com>
2015-07-03 00:52:15 -0700

    Add RemoveProfilePayload

---
+++ lib/ios_config_profile/device/remove_profile_payload.rb
@@ -0,0 +1,20 @@
+class IOSConfigProfile::RemoveProfilePayload < Hash
+  include IOSConfigProfile::BasicPayload
+
+  attr_accessor :device_profile_identifier
+
+  def initialize(device_profile_identifier)
+    self.device_profile_identifier = device_profile_identifier
+    require_attributes :device_profile_identifier
+    merge! remove_profile_payload
+  end
+
+  private
+
+  def remove_profile_payload
+    {
+      'RequestType' => 'RemoveProfile',
+      'Identifier' => device_profile_identifier
+    }
+  end
+end
---
+++ spec/device/remove_profile_payload_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe IOSConfigProfile::RemoveProfilePayload do
+  let(:remove_profile_payload_attributes) do
+    {
+      "RequestType" => "RemoveProfile",
+      "Identifier" => 'profile-identifier'
+    }
+  end
+
+  subject { IOSConfigProfile::RemoveProfilePayload.new 'profile-identifier'}
+
+  it { is_expected.to eq(remove_profile_payload_attributes) }
+end
