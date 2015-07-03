66b81f42115cad50e21b714f22c2ec60896c847c
Albert Wang <albert@cellabus.com>
2015-07-02 17:45:12 -0700

    Add LockDevicePayload

---
+++ lib/ios_config_profile/device/lock_device_payload.rb
@@ -0,0 +1,15 @@
+class IOSConfigProfile::LockDevicePayload < Hash
+  include IOSConfigProfile::BasicPayload
+
+  def initialize
+    merge! lock_device_payload
+  end
+
+  private
+
+  def lock_device_payload
+    {
+        'RequestType' => 'DeviceLock',
+    }
+  end
+end
