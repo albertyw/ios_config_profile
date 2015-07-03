f6bb3d4ce88185351d1922f1f6b10b5889560a44
Albert Wang <albert@cellabus.com>
2015-07-02 18:01:18 -0700

    Add SetDeviceNamePayload

---
+++ lib/ios_config_profile/device/set_device_name_payload.rb
@@ -0,0 +1,20 @@
+class IOSConfigProfile::SetDeviceNamePayload < Hash
+  include IOSConfigProfile::BasicPayload
+
+  attr_accessor :new_device_name
+
+  def initialize(new_device_name)
+    self.new_device_name = new_device_name
+    require_attributes :new_device_name
+    merge! set_device_name_payload
+  end
+
+  private
+
+  def set_device_name_payload
+    {
+        'RequestType' => 'Settings',
+        'Settings' => [{ 'Item' => 'DeviceName', 'DeviceName' => new_device_name}]
+    }
+  end
+  end
