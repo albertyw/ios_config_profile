840e08fdc66b1739e05d18683efd3c43b180e85e
Albert Wang <albert@cellabus.com>
2015-07-03 00:36:06 -0700

    Add InstallBookPayload

---
+++ lib/ios_config_profile/content/install_book_payload.rb
@@ -0,0 +1,21 @@
+class IOSConfigProfile::InstallBookPayload < Hash
+  include IOSConfigProfile::BasicPayload
+
+  attr_accessor :itunes_store_id
+
+  def initialize(itunes_store_id)
+    self.itunes_store_id = itunes_store_id
+    require_attributes :itunes_store_id
+    merge! install_book_payload
+  end
+
+  private
+
+  def install_book_payload
+    {
+        'RequestType' => 'InstallMedia',
+        'iTunesStoreID' => itunes_store_id,
+        'MediaType' => 'Book'
+    }
+  end
+end
